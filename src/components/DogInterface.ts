4d9131d0a26b9300f62a5517b97352e69e14f6a2
Christian Sophocleous <c-sophocleous@hotmail.com>
2022-05-03 12:45:07 +0100

    fetched dog

--- src/components/DogInterface.ts
+++ src/components/DogInterface.ts
@@ -1,4 +1,4 @@
 export default interface DogInterface {
-    message:string, 
-    status:string
+  message: string;
+  status: string;
 }
--- src/components/MainComponent.tsx
+++ src/components/MainComponent.tsx
@@ -1,14 +1,23 @@
-import { useState } from "react";
-import axios from "axios"
+import { useState, useEffect } from "react";
+import axios from "axios";
 import { config } from "dotenv";
 import DogInterface from "./DogInterface";
 
-
 export default function Main(): JSX.Element {
-    const [dog, setDog] = useState<DogInterface>()
+  const [dog1, setDog1] = useState<DogInterface>({ message: "", status: "" });
+
+  useEffect(() => {
+    const fetchDog1 = async () => {
+      const response = await fetch("https://dog.ceo/api/breeds/image/random");
+      const jsonBody: DogInterface = await response.json();
+      setDog1(jsonBody);
+    };
+    fetchDog1();
+  }, []);
 
   return (
     <>
+      <img src={dog1.message} />
     </>
   );
 }
