eeb52cd8316590b95d640b68a57c2b944966c1ba
Zahra <bghzahra@outlook.com>
2022-05-03 15:58:12 +0100

    added onclick handler for vote

--- src/components/MainComponent.tsx
+++ src/components/MainComponent.tsx
@@ -2,10 +2,13 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { config } from "dotenv";
 import DogInterface from "./DogInterface";
+import getBreed from "../utils/getBreed";
 
 export default function Main(): JSX.Element {
   const [dog1, setDog1] = useState<DogInterface>({ message: "", status: "" });
   const [dog2, setDog2] = useState<DogInterface>({ message: "", status: "" });
+
+  let allBreeds = [];
 
   useEffect(() => {
     const fetchDog1 = async () => {
@@ -22,10 +25,18 @@ export default function Main(): JSX.Element {
     fetchDog2();
   }, []);
 
+  const handleVoteDog = (link:string) => {
+    const breed = getBreed(link)
+    
+  }
+
   return (
     <>
-      <img src={dog1.message} />
-      <img src ={dog2.message} />
+    <div>
+    <h3>Click on your favourite dog to vote!</h3>
+      <img src={dog1.message} onClick={() => handleVoteDog(dog1.message)}/>
+      <img src ={dog2.message} onClick={() => handleVoteDog(dog2.message)}/>
+    </div>
     </>
   );
 }
