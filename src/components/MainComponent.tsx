3f302b096fab4d5bdfa07ff8cd8912c161f4de27
Christian Sophocleous <c-sophocleous@hotmail.com>
2022-05-03 12:46:55 +0100

    fetched 2 dogs

--- src/components/MainComponent.tsx
+++ src/components/MainComponent.tsx
@@ -5,6 +5,7 @@ import DogInterface from "./DogInterface";
 
 export default function Main(): JSX.Element {
   const [dog1, setDog1] = useState<DogInterface>({ message: "", status: "" });
+  const [dog2, setDog2] = useState<DogInterface>({ message: "", status: "" });
 
   useEffect(() => {
     const fetchDog1 = async () => {
@@ -12,12 +13,19 @@ export default function Main(): JSX.Element {
       const jsonBody: DogInterface = await response.json();
       setDog1(jsonBody);
     };
+    const fetchDog2 = async () => {
+      const response = await fetch("https://dog.ceo/api/breeds/image/random");
+      const jsonBody: DogInterface = await response.json();
+      setDog2(jsonBody);
+    };
     fetchDog1();
+    fetchDog2();
   }, []);
 
   return (
     <>
       <img src={dog1.message} />
+      <img src ={dog2.message} />
     </>
   );
 }
