6331ef2deff08b19b7ccf8fb4ac6c5d84b0b071d
Zahra <bghzahra@outlook.com>
2022-05-03 12:28:52 +0100

    added use state for dog api

--- src/components/MainComponent.tsx
+++ src/components/MainComponent.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import axios from "axios"
 import { config } from "dotenv";
+import DogInterface from "./DogInterface";
 
 
 export default function Main(): JSX.Element {
-
+    const [dog, setDog] = useState<DogInterface>()
 
   return (
     <>
