8da09a9eb3660bd46d23f8295408f9143a5d33a3
Zahra <bghzahra@outlook.com>
2022-05-03 12:24:04 +0100

    added relevant packages

--- src/components/MainComponent.tsx
+++ src/components/MainComponent.tsx
@@ -1,3 +1,13 @@
+import { useState } from "react";
+import axios from "axios"
+import { config } from "dotenv";
+
+
 export default function Main(): JSX.Element {
-  return <></>;
+
+
+  return (
+    <>
+    </>
+  );
 }
