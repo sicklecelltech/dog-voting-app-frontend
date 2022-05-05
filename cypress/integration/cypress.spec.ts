865af73facca2d0602b28b99dcde146f10df33e5
Nobeen <n.islam.1@outlook.com>
2022-05-05 17:30:09 +0100

    Didn't save the comment delete on the test file for cypress

--- cypress/integration/cypress.spec.ts
+++ cypress/integration/cypress.spec.ts
@@ -11,21 +11,9 @@ describe("Testing the dog app", () => {
 
     it("Clicks a dog picture and gets a new picture", () => {
         cy.get("img").eq(0).invoke("attr", "src").then((firstSrc) => {
-
             console.log("THis is cypress", firstSrc)
         })
-        // cy.get(".dog").eq(0).click()
-        // let src2 = ""
-        // cy.get("img").eq(0).invoke("attr", "src").then((secondSrc) => {
-        //     src2 = secondSrc
-        // })
 
-
-        // const test = cy.get("img").eq(0).should('have.attr', 'src').should('include', 'https')
-        // console.log("This is scypress,", test)
-        // cy.get(".dog").eq(0).click()
-        // cy.get("img").eq(0).should('have.attr', 'src').should('include', 'https')
-        //if link image is different test passes
 
     })
 })
