96ce89200610c65f35339cd9dc05d906667f30ca
Zahra <bghzahra@outlook.com>
2022-05-09 16:32:55 +0100

    testing bug with Aluka

--- src/components/MainComponent.tsx
+++ src/components/MainComponent.tsx
@@ -1,9 +1,10 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import axios from "axios";
 import DogInterface from "./DogInterface";
 import DataBaseDogs from "./DatabaseDogInterface";
 import getBreed from "../utils/getBreed";
 import audio from '../sounds/bite.mp3'
+import { link } from "fs";
 
 
 export default function Main(): JSX.Element {
@@ -12,6 +13,7 @@ export default function Main(): JSX.Element {
   const [dataBaseDogs, setDataBaseDogs] = useState<DataBaseDogs[]>([]);
   const [toggle, setToggle] = useState<boolean>(false);
   const [userVote, setUserVote] = useState<number>(0);
+  const [leaderLinks, setLeaderLinks] = useState<string[]>([])
 
   const myAudio = new Audio(audio);
 
@@ -32,7 +34,6 @@ export default function Main(): JSX.Element {
         "https://tichnozar-dog-voting-app.herokuapp.com/breeds"
       );
       setDataBaseDogs(response.data);
-      console.log(dataBaseDogs);
     };
     fetchDog1();
     fetchDog2();
@@ -68,6 +69,50 @@ export default function Main(): JSX.Element {
       );
     }
   };
+
+  const leaderDogs = dataBaseDogs.slice(0,3)
+  const breeds = leaderDogs.map((dog) => dog.dogbreed)
+
+  const linksArray:string[] = [] 
+
+ 
+ 
+    const fetchLeaderLinks = async () => {
+      console.log(breeds)
+      for (const breed of breeds) {
+        if (breed.includes("-")) {
+          const urlBreed = breed.replace('-', '/')
+          const response = await fetch(`https://dog.ceo/api/breed/${urlBreed}/images/random`)
+          const jsonBody: DogInterface = await response.json();
+          const imageLink = jsonBody
+          console.log("this is the sub breed img link", imageLink)
+         // setLeaderLinks([...leaderLinks, imageLink.message])
+          linksArray.push(imageLink.message)
+          console.log("this is the sub breed links array",linksArray)
+
+        } else {
+          const response = await fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
+          const jsonBody: DogInterface = await response.json();
+          const imageLink = jsonBody
+          console.log("this is the breed img link", imageLink)
+         // setLeaderLinks([...leaderLinks, imageLink.message])
+          linksArray.push(imageLink.message)
+          console.log("this is the breed links array",linksArray)
+        } 
+      } 
+    }
+
+    useEffect(() => {
+       fetchLeaderLinks()
+       console.log("this is links array in use effect", linksArray)
+       setLeaderLinks(linksArray)
+  }, [linksArray])  
+
+
+
+  const images = linksArray.map((dog) => <img src={dog} key ={dog}/>)
+  
+
 
   return (
     <>
@@ -111,6 +156,7 @@ export default function Main(): JSX.Element {
             .slice(0, 9)}
         </table>
       </div>
+      <div>{images}</div>
     </>
   );
 }
